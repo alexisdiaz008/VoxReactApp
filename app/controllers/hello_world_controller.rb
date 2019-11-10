701315db3000a91c92eee283647a228cc376dcbe
alexisdiaz008 <lexmasta@gmail.com>
2019-11-10 15:31:24 -0500

    rework layouts

--- app/controllers/hello_world_controller.rb
+++ app/controllers/hello_world_controller.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 class HelloWorldController < ApplicationController
-  layout "hello_world"
 
   def index
     @hello_world_props = { name: "Stranger" }
     @audio_clip = AudioClip.new
+  end
+
+  def create_audio_clip
   end
 end
