f944172d33a2dc60af06655616e82ca46c3e1a63
alexisdiaz008 <lexmasta@gmail.com>
2019-11-09 23:15:42 -0500

    seed notes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Generate audioclips
+# AudioClip.create(name:"Test clip", clip: clip_from_public_folder )
